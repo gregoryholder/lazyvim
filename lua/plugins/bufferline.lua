d940e9131ba40ce7676e170aa159ed264013c3f0
Gregory Holder <gregory.holder@equans.com>
2025-04-12 14:50:35 +0200

    Initial commit

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,9 @@
+return {
+  "akinsho/bufferline.nvim",
+  opts = {
+    options = {
+      always_show_bufferline = true,
+      max_name_length = 50
+    }
+  }
+}
---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,16 @@
+return {
+ "snacks.nvim", 
+  opts = {
+    picker  = { 
+    sources = {
+      explorer = {
+        layout = {
+            layout = {
+              position = "right"
+            }
+          }
+        }
+      }
+    }
+  }
+}
