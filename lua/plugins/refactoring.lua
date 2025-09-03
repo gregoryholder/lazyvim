346cc99e307bb0e6634715a003b51d47a619ee47
Gregory Holder <gregory.holder@equans.com>
2025-09-03 14:39:57 +0200

    refactoring add custom keybinds

--- lua/plugins/refactoring.lua
+++ lua/plugins/refactoring.lua
@@ -14,4 +14,30 @@ return {
       },
     },
   },
+  keys = {
+    {
+      "<leader>rP",
+      function()
+        require("refactoring").debug.printf({ below = false })
+      end,
+      desc = "Debug Print",
+      mode = {"n", "x"},
+    },
+    {
+      "<leader>rp",
+      function()
+        require("refactoring").debug.print_var({ normal = true })
+      end,
+      desc = "Debug Print Variable",
+      mode = {"n", "x"},
+    },
+    {
+      "<leader>rr",
+      function()
+        require("refactoring").select_refactor()
+      end,
+      desc = "Pick",
+      mode = {"n", "x"},
+    },
+  },
 }
