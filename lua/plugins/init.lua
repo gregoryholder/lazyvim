6e848e7d07332d71a8c606dac744ba5115d26677
Gregory Holder <gregory.holder@equans.com>
2025-09-03 14:36:58 +0200

    dim inactive

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -27,6 +27,12 @@ return {
     name = "catppuccin",
     priority = 1000,
     opts = {
+      dim_inactive = {
+        enabled = true,
+      },
+      styles = {
+        comments = {},
+      },
       custom_highlights = function(colors)
         return {
           WinSeparator = { fg = colors.flamingo },
