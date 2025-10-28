31fd12a9d04d9bb3fcf4c0545f9a64a3952e5220
Gregory Holder <gregory.holder@equans.com>
2025-10-28 16:47:23 +0100

    tweak doxygen config for neogen
    param -> param[in]

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -112,4 +112,38 @@ return {
       { "S", mode = {"n", "x", "o", false }},
     },
   },
+  {
+      "danymat/neogen",
+      -- config = true,
+      -- Uncomment next line if you want to follow only stable versions
+      -- version = "*" 
+      opts = function (_)
+        local i = require("neogen.types.template").item
+        return {
+          languages = {
+            cpp = {
+              template = {
+                doxygen = {
+                  { nil, "/**", { no_results = true, type = { "func", "file", "class" } } },
+                  { nil, " * @file", { no_results = true, type = { "file" } } },
+                  { nil, " * @brief $1", { no_results = true, type = { "func", "file", "class" } } },
+                  { nil, " */", { no_results = true, type = { "func", "file", "class" } } },
+                  { nil, "", { no_results = true, type = { "file" } } },
+
+                  { nil, "/**", { type = { "func", "class", "type" } } },
+                  { i.ClassName, " * @class %s", { type = { "class" } } },
+                  { i.Type, " * @typedef %s", { type = { "type" } } },
+                  { nil, " * @brief $1", { type = { "func", "class", "type" } } },
+                  { nil, " *", { type = { "func", "class", "type" } } },
+                  { i.Tparam, " * @tparam %s $1" },
+                  { i.Parameter, " * @param[in] %s $1" },
+                  { i.Return, " * @return $1" },
+                  { nil, " */", { type = { "func", "class", "type" } } },
+                },
+              },
+            },
+          },
+        }
+      end
+  }
 }
