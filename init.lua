fc2a957ab2bfb09bf350a502bde3f6dc80d05e14
Gregory Holder <gregory.holder@equans.com>
2025-11-05 14:10:36 +0100

    focus wezterm pane on BufEnter

--- init.lua
+++ init.lua
@@ -2,3 +2,9 @@
 require("config.lazy")
 vim.cmd("set spelllang=fr,en ")
 vim.cmd("set noswapfile")
+
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+  callback = function()
+    os.execute("wezterm cli activate-pane")
+  end,
+})
