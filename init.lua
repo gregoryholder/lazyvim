dfbb268920c615ac2bac174430ad1b10f4c7496d
Gregory Holder <gregory.holder@equans.com>
2026-01-08 13:33:46 +0100

    inlay stuff

--- init.lua
+++ init.lua
@@ -1,10 +1,36 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.xml_gen = {
+  install_info = {
+    url = "~/workspace/fun/tree-sitter-xml-gen", -- local path or git repo
+    files = {"src/parser.c"}, -- note that some parsers also require src/scanner.c or src/scanner.cc
+    -- optional entries:
+    branch = "main", -- default branch in case of git repo if different from master
+    generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+    requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+  },
+  filetype = "zu", -- if filetype does not match the parser name
+}
+
 vim.cmd("set spelllang=fr,en ")
 vim.cmd("set noswapfile")
+
+-- require("xml_macro_lsp_helper").setup()
+--
+package.loaded.xml_delay_inlay = nil
+local delay_inlay = require("xml_delay_inlay")
+vim.keymap.set("n", "<leader>ue", delay_inlay.toggle_inlays, { desc = "Toggle XML Delay Inlays" })
 
 vim.api.nvim_create_autocmd({ "BufEnter" }, {
   callback = function()
     os.execute("wezterm cli activate-pane")
   end,
 })
+
+vim.opt.path:append("Affaires/*/tactileo_ucineo11/mineo/res/")
+
+
+-- vim.lsp.enable('protols')
--- lua/xml_delay_inlay.lua
+++ lua/xml_delay_inlay.lua
@@ -3,6 +3,30 @@ local M = {}
 local ns_id = vim.api.nvim_create_namespace("xml_delay_inlays")
 local enabled = false
 
+local function hex_msg_type_to_string (msg_id)
+  local lookup = {
+     ["0000"]= "InitialisationDuLecteur",
+     ["0001"]= "InhibitionDeLaLectureDesBadges",
+     ["0002"]= "CommandeDeMaintenance",
+     ["0003"]= "CommandeDAllumageDUneLED",
+     ["0004"]= "CommandeDExtinctionDUneLED",
+     ["0005"]= "CommandeDeClignotementDUneLED",
+     ["0006"]= "CommandeDeClignotementRapideDUneLED",
+     ["0007"]= "AcquittementOut",
+     ["0008"]= "NonAcquittementOut",
+     ["0009"]= "QuiEsTu",
+     ["000a"]= "AsTuQuelqueChoseADire",
+     ["000b"]= "CommandeDuBuzzer",
+     ["000c"]= "CommandeCompleteSimplifiee",
+     ["20"]= "LectureDUnBadge",
+     ["23"]= "IdentificationDuLecteur",
+     ["24"]= "AcquittementIn",
+     ["25"]= "NonAcquittementIn",
+  }
+  return lookup[msg_id]
+end
+
+
 local function show_inlays()
   vim.api.nvim_buf_clear_namespace(0, ns_id, 0, -1)
   local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
@@ -37,7 +61,24 @@ local function show_inlays()
     local line_offset = 1
 
     while true do
-      local match_start, match_end = string.find(line, '"([0-9a-fA-F]+)"', line_offset)
+      local match_start, match_end = string.find(line, 'valeur *= *"', line_offset)
+      if match_start == nil then
+        match_start, match_end = string.find(line, 'rx_hex *= *"', line_offset)
+      end
+      if match_start == nil then
+        match_start, match_end = string.find(line, 'Data *= *"', line_offset)
+      end
+
+      if match_start == nil then
+        break
+      end
+
+      line_offset = match_start
+
+      local cursor = vim.api.nvim_win_get_cursor(0)
+      local row, col = cursor[1], cursor[2]
+
+      match_start, match_end = string.find(line, '"([0-9a-fA-F*]+)"', line_offset)
       if match_start == nil then
         break
       end
@@ -46,40 +87,60 @@ local function show_inlays()
 
       local hex_str = line:sub(match_start + 1, match_end - 1)
 
-      local cursor = vim.api.nvim_win_get_cursor(0)
-      local row, col = cursor[1], cursor[2]
-
-      local ascii = {}
-      for i = 1, #hex_str, 2 do
-        local byte = tonumber(hex_str:sub(i, i + 1), 16)
-        if byte >= 32 and byte <= 127 then
-          ascii[#ascii + 1] = string.char(byte)
-        else
-          ascii[#ascii + 1] = "."
-        end
+      local found = string.match(hex_str, '100205..(00..)')
+      if not found then
+        found = string.match(hex_str, '100207..(..)')
       end
 
-      local byte_index = math.floor((col - start_pos) / 2) + 1
-      if byte_index >= 1 and byte_index <= #ascii and row == line_num then
-        local before = table.concat(ascii, "", 1, byte_index - 1)
-        local highlight = ascii[byte_index] or ""
-        local after = table.concat(ascii, "", byte_index + 1)
-        vim.api.nvim_buf_set_extmark(0, ns_id, line_num - 1, #line, {
-          virt_text = {
-            { " [" .. before, "Comment" },
-            { highlight, "WarningMsg" },
-            { after .. "] ", "Comment" },
-          },
-          virt_text_pos = "inline",
-        })
-      else
-        vim.api.nvim_buf_set_extmark(0, ns_id, line_num - 1, #line, {
-          virt_text = {
-            { " [" .. table.concat(ascii, "", 1) .. "] ", "Comment" },
-          },
-          virt_text_pos = "inline",
-        })
-        -- end
+
+      -- 1002077f230000000010033b
+
+      -- if #hex_str % 2 == 0 then
+      if true then
+        local ascii = {}
+        for i = 1, #hex_str, 2 do
+          local byte = tonumber(hex_str:sub(i, i + 1), 16)
+          if byte and byte >= 32 and byte <= 126 then
+            ascii[#ascii + 1] = string.char(byte)
+          else
+            ascii[#ascii + 1] = "."
+          end
+        end
+
+        local byte_index = math.floor((col - start_pos) / 2) + 1
+        if byte_index >= 1 and byte_index <= #ascii and row == line_num then
+          local before = table.concat(ascii, "", 1, byte_index - 1)
+          local highlight = ascii[byte_index] or ""
+          local after = table.concat(ascii, "", byte_index + 1)
+          vim.api.nvim_buf_set_extmark(0, ns_id, line_num - 1, #line, {
+            virt_text = {
+              { " [" .. before, "Comment" },
+              { highlight, "WarningMsg" },
+              { after .. "] ", "Comment" },
+            },
+            virt_text_pos = "inline",
+          })
+        else
+          vim.api.nvim_buf_set_extmark(0, ns_id, line_num - 1, #line, {
+            virt_text = {
+              { " [" .. table.concat(ascii, "", 1) .. "] ", "Comment" },
+            },
+            virt_text_pos = "inline",
+          })
+          -- end
+        end
+        if found then
+          local str_type = hex_msg_type_to_string(found)
+          if not str_type then
+            str_type = "???"
+          end
+          vim.api.nvim_buf_set_extmark(0, ns_id, line_num - 1, #line, {
+              virt_text = {
+                { " [" .. str_type .. "] ", "Comment" },
+              },
+            virt_text_pos = "inline",
+          })
+        end
       end
       line_offset = match_end + 1
     end
