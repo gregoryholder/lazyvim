e0d3e4c139aeaf9ca9d7a1aaa3678774ada2e5e2
Gregory Holder <gregory.holder@equans.com>
2026-05-05 14:39:35 +0200

    Add trailing whitespace highlighting and clipboard settings
    - Enable clipboard sync with unnamedplus
    - Add highlighting for trailing whitespace
    - Add StripTrailingWhitespace command for cleanup
    - Include commented reference to snacks toggle option

--- init.lua
+++ init.lua
@@ -51,6 +51,7 @@ vim.cmd("set noswapfile")
 --
 package.loaded.xml_delay_inlay = nil
 local delay_inlay = require("tae_inlays")
+-- require("snacks").toggle.option()
 vim.keymap.set("n", "<leader>ue", delay_inlay.toggle_inlays, { desc = "Toggle XML Delay Inlays" })
 
 vim.api.nvim_create_autocmd({ "BufEnter" }, {
@@ -88,3 +89,69 @@ vim.diagnostic.config({
     border = "rounded",
   },
 })
+vim.o.clipboard = 'unnamedplus'
+
+-- Highlight trailing whitespace
+vim.api.nvim_set_hl(0, "TrailingWhitespace", { bg = "#AF00FF", fg = "#FFFFFF" })
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile", "InsertLeave" }, {
+  callback = function()
+    vim.fn.matchadd("TrailingWhitespace", "\\s\\+$", -1)
+  end,
+})
+
+-- Strip trailing whitespace command
+local function strip_trailing_whitespace(opts)
+  local confirm = opts.args == "-confirm"
+  local start_line = opts.line1
+  local end_line = opts.line2
+  
+  local cmd = string.format("%d,%ds/\\s\\+$//e%s", start_line, end_line, confirm and "c" or "")
+  vim.cmd(cmd)
+end
+
+vim.api.nvim_create_user_command("StripTrailingWhitespace", strip_trailing_whitespace, {
+  nargs = "?",
+  range = "%",
+  complete = function(arg_lead, cmd_line, cursor_pos)
+    if arg_lead:match("^-") then
+      return { "-confirm" }
+    end
+    return {}
+  end,
+})
+
+-- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+-- vim.schedule(function()
+--   vim.o.clipboard:append('unnamedplus')
+--
+--   -- Fix "waiting for osc52 response from terminal" message
+--   -- https://github.com/neovim/neovim/issues/28611
+--
+--   if vim.env.SSH_TTY ~= nil then
+--     -- Set up clipboard for ssh
+--
+--     local function my_paste(_)
+--       return function(_)
+--         local content = vim.fn.getreg('"')
+--         return vim.split(content, '\n')
+--       end
+--     end
+--
+--     vim.g.clipboard = {
+--       name = 'OSC 52',
+--       copy = {
+--         ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+--         ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+--       },
+--       paste = {
+--         -- No OSC52 paste action since wezterm doesn't support it
+--         -- Should still paste from nvim
+--         ['+'] = my_paste('+'),
+--         ['*'] = my_paste('*'),
+--       },
+--     }
+--   end
+-- end)
